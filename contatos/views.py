47fe5086a395368c89b5a7e6e795b9e9a9c5c650
Werberty Alexandre <werberty.alexandre@gmail.com>
2022-04-23 12:19:32 -0300

    Erro404, condicionais e paginação

--- contatos/views.py
+++ contatos/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Contato
 
 
 def index(request):
     contatos = Contato.objects.all()
+    paginator = Paginator(contatos, 5)
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
     return render(request, 'contatos/index.html', {
         'contatos': contatos
+    })
+
+
+def ver_contato(request, contato_id):
+    # contato = Contato.objects.get(id=contato_id)
+    contato = get_object_or_404(Contato, id=contato_id)
+    return render(request, 'contatos/ver_contato.html', {
+        'contato': contato
     })
